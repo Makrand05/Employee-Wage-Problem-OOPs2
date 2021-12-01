3d0e19bbf95d08320798cba92d6001efd4e94b2b
Makrand <makrandshingare@gmail.com>
2021-12-01 22:12:39 +0530

    Added: calculated employee daily wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,25 +1,36 @@
 public class EmployeeWage {
 
     int status;
+    int RATE_PER_HOUR = 20;
+    int DAY_HOURS = 8;
+    int wage = 0;
 
-    private void checkStstus() {
+    private void checkStatus() {
 
-        this.status=((int)(Math.random()*10)%2);
-        if(status==1) {
+        this.status = ((int) (Math.random() * 10) % 2);
+        if (status == 1) {
             System.out.println("Employee is present");
-        }
-        else
+        } else
             System.out.println("Employee is absent");
     }
 
     public static void main(String[] args) {
 
-        EmployeeWage employeeWage=new EmployeeWage();
-        employeeWage.checkStstus();
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.checkStatus();
+        employeeWage.calculateDailyWage();
 
 
     }
 
+    private void calculateDailyWage() {
+        if(status==1){
+            wage=DAY_HOURS*RATE_PER_HOUR;
+        }
+        else
+            wage=0;
+        System.out.println("Total Wage of Employee is : "+wage);
+    }
 
 
 }
