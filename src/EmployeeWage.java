71cd05fa347e8f91474e2604a740c1647a98c9e2
Makrand <makrandshingare@gmail.com>
2021-12-01 23:51:16 +0530

    Added : Calculated employee wage for the multiple company

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,21 +1,25 @@
 import org.openqa.selenium.json.JsonOutput;
 
+import java.util.Scanner;
+
 public class EmployeeWage {
 
     int status;
-    int RATE_PER_HOUR = 20;
-    int MAX_WORKING_HOURS = 100;
-    int wage = 0;
+    int RATE_PER_HOUR;
+    int MAX_WORKING_HOURS;
     final int PART_TIME = 2;
     final int FULL_TIME = 1;
     int empHours = 0;
     int totalMonthlyWage = 0;
-    int workingDayInMonth = 20;
+    int workingDayInMonth;
     int totalWorkingHours = 0;
-
+    EmployeeWage(int workingRate, int workingHoursInMonth, int workingDayInMonth){
+    this.RATE_PER_HOUR=workingRate;
+    this.MAX_WORKING_HOURS=workingHoursInMonth;
+    this.workingDayInMonth=workingDayInMonth;
+    }
     //Check whether the employee is present or not
     private int checkStatus() {
-
         this.status = ((int) (Math.random() * 10) % 3);
         switch (status) {
             case FULL_TIME:
@@ -50,7 +54,15 @@ public class EmployeeWage {
     }
 
     public static void main(String[] args) {
-        EmployeeWage employeeWage = new EmployeeWage();
+        //for the working hour rate 20, working hour in month- 100 working hour in month 20
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Working day in month : ");
+        int workingDayInMonth=sc.nextInt();
+        System.out.print("Enter the Hours day in month : ");
+        int workingHoursInmonth=sc.nextInt();
+        System.out.print("Enter the rate per hours: ");
+        int ratePerHours=sc.nextInt();
+        EmployeeWage employeeWage = new EmployeeWage(ratePerHours,workingHoursInmonth,workingDayInMonth);
         employeeWage.calculateDailyWage();
         employeeWage.wageForMonth();
         employeeWage.printOutput();
