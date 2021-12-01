b7737f9709966dd4897534f9a2c9ab2bf147e8e9
Makrand <makrandshingare@gmail.com>
2021-12-01 22:02:35 +0530

    Added : check the employee is present or not

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,25 @@
 public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+
+    int status;
+
+    private void checkStstus() {
+
+        this.status=((int)(Math.random()*10)%2);
+        if(status==1) {
+            System.out.println("Employee is present");
+        }
+        else
+            System.out.println("Employee is absent");
     }
+
+    public static void main(String[] args) {
+
+        EmployeeWage employeeWage=new EmployeeWage();
+        employeeWage.checkStstus();
+
+
+    }
+
+
+
 }
