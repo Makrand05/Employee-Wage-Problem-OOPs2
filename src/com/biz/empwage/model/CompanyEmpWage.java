2a535c7d9890b9eb93902a89c3d6e0e4ceb99146
Makrand <makrandshingare@gmail.com>
2021-12-04 22:36:15 +0530

    Refactor : To show the total emp wage and Daily wage

--- src/com/biz/empwage/model/CompanyEmpWage.java
+++ src/com/biz/empwage/model/CompanyEmpWage.java
@@ -1,16 +1,23 @@
 package com.biz.empwage.model;
 
+import java.util.Arrays;
+
 public class CompanyEmpWage {
     String compName;
     public int RATE_PER_HOUR;
     public int MAX_WORKING_HOURS;
-   public int workingDayInMonth;
+    public int workingDayInMonth;
+   public int monthlyEmpWage;
+
+    public int empDailyWage[];
 
     public CompanyEmpWage(String compName, int RATE_PER_HOUR, int MAX_WORKING_HOURS, int workingDayInMonth) {
         this.compName = compName;
         this.RATE_PER_HOUR = RATE_PER_HOUR;
         this.MAX_WORKING_HOURS = MAX_WORKING_HOURS;
         this.workingDayInMonth = workingDayInMonth;
+        this.empDailyWage = new int[workingDayInMonth];
+
     }
 
     @Override
@@ -20,6 +27,8 @@ public class CompanyEmpWage {
                 ", RATE_PER_HOUR=" + RATE_PER_HOUR +
                 ", MAX_WORKING_HOURS=" + MAX_WORKING_HOURS +
                 ", workingDayInMonth=" + workingDayInMonth +
+                ", monthlyEmpWage=" + monthlyEmpWage +
+                ", empDailyWage=" + Arrays.toString(empDailyWage) +
                 '}';
     }
 }
--- src/com/biz/empwage/service/EmpWageBuilder.java
+++ src/com/biz/empwage/service/EmpWageBuilder.java
@@ -1,36 +1,31 @@
 package com.biz.empwage.service;
-
 import com.biz.empwage.interface1.IEmployeeWage;
 import com.biz.empwage.model.CompanyEmpWage;
-
 import java.util.ArrayList;
 
 public class EmpWageBuilder implements IEmployeeWage {
 
     int status;
-
     final int PART_TIME = 2;
     final int FULL_TIME = 1;
     int empHours = 0;
     int totalMonthlyWage = 0;
     int totalWorkingHours = 0;
-    int totalEmployeeWage;
-    //CompanyEmpWage[] companyEmpArray = new CompanyEmpWage[5];
-    ArrayList <CompanyEmpWage> empCompanyArraylist=new ArrayList<>();
+    ArrayList<CompanyEmpWage> empCompanyArraylist = new ArrayList<>();
+
 
     public static void main(String[] args) {
-
 
         IEmployeeWage empWageBuilder = new EmpWageBuilder();
         empWageBuilder.addCompanyEmpWage("ABC", 20, 100, 20);
         empWageBuilder.addCompanyEmpWage("Apple", 25, 120, 25);
         empWageBuilder.empWageArray();
-    }
 
+    }
     public void empWageArray() {
         for (int i = 0; i < empCompanyArraylist.size(); i++) {
-            int totalWage = calculateDailyWage(empCompanyArraylist.get(i));
-            empCompanyArraylist.get(totalWage);
+            CompanyEmpWage totalWage = calculateDailyWage(empCompanyArraylist.get(i));
+            System.out.println(empCompanyArraylist.get(i));
         }
     }
 
@@ -39,34 +34,32 @@ public class EmpWageBuilder implements IEmployeeWage {
         empCompanyArraylist.add(companyEmpWage);
     }
 
-    //Check whether the employee is present or not
-
-
     //Calculated Employ wage
-    private int calculateEmpWage(int RATE_PER_HOUR) {
-        this.status = ((int) (Math.random() * 10) % 3);
-        switch (status) {
-            case FULL_TIME:
-                empHours = 8;
-                totalWorkingHours += 8;
-                break;
-            case PART_TIME:
-                empHours = 4;
-                totalWorkingHours += 4;
-                break;
-            default:
-                empHours = 0;
-        }
-        return (empHours * RATE_PER_HOUR);
-    }
-
-    private int calculateDailyWage(CompanyEmpWage companyEmpWage) {
+    private CompanyEmpWage calculateDailyWage(CompanyEmpWage companyEmpWage) {
         int day = 0;
+        int index = 0;
+        int dayCounter = 0;
         while (day < companyEmpWage.workingDayInMonth && totalWorkingHours < companyEmpWage.MAX_WORKING_HOURS) {
-            totalMonthlyWage += calculateEmpWage(companyEmpWage.RATE_PER_HOUR);
+            this.status = ((int) (Math.random() * 10) % 3);
+            switch (status) {
+                case FULL_TIME:
+                    empHours = 8;
+                    totalWorkingHours += 8;
+                    break;
+                case PART_TIME:
+                    empHours = 4;
+                    totalWorkingHours += 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            int empWage = empHours * companyEmpWage.RATE_PER_HOUR;
+            companyEmpWage.empDailyWage[dayCounter++] = empWage;
+            totalMonthlyWage += empWage;
             day++;
         }
-        return totalMonthlyWage;
+        companyEmpWage.monthlyEmpWage=totalMonthlyWage;
+        return companyEmpWage;
     }
 
 }
